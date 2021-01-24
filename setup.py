a0442fd3be8e704b3a0f4feef228a4be1b88d519
prays <praysomaldo@ymail.com>
2021-01-24 20:18:28 +0700

    remove unuses library in requirements and setup.py

--- setup.py
+++ setup.py
@@ -20,5 +20,9 @@ setuptools.setup(
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.5.5',
+<<<<<<< HEAD
     install_requires=["numpy>=1.14.0", "pandas>=0.23.4", "tqdm>=4.30.0", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=3.4.4", "tensorflow>=1.9.0", "keras>=2.2.0", "Flask>=1.1.2", "mtcnn>=0.1.0", "retina-face>=0.0.1"]
+=======
+    install_requires=["numpy>=1.14.0", "pandas>=0.23.4", "tqdm>=4.30.0", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=3.4.4", "tensorflow>=1.9.0", "Flask>=1.1.2", "mtcnn>=0.1.0"]
+>>>>>>> remove unuses library in requirements and setup.py
 )
